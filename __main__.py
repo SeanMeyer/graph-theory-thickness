2a0733f0b85f142d725c071d5c56091aae105360
Scott Meyer <Scott.R.Meyer@gmail.com>
2018-03-25 00:12:30 -0600

    Initial files.

---
+++ __main__.py
@@ -0,0 +1,8 @@
+
+
+##################--------- DRIVER CODE  -------###################### 
+
+if __name__ != '__main__':
+    print('Please run as a self-conatined program')
+else:
+    print("Hello world, I am main.")
